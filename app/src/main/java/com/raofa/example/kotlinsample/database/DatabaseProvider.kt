ac5da86ef7a119b6bec702f570a6ad268bb1b77c
raofa <1246967931@qq.com>
2020-05-24 22:42:19 +0800

    5.24 进度提交

--- app/src/main/java/com/raofa/example/kotlinsample/database/DatabaseProvider.kt
+++ app/src/main/java/com/raofa/example/kotlinsample/database/DatabaseProvider.kt
@@ -2,6 +2,7 @@ package com.raofa.example.kotlinsample.database
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 
@@ -11,31 +12,60 @@ class DatabaseProvider : ContentProvider() {
     private val bookItem = 1
     private val categoryDir = 2
     private val categoryItem = 3
+    private val authority = "com.raofa.example.kotlinsample.provider"
+    private var dbHelper : MyDatabaseHelper? = null
+
+    private val uriMatcher by lazy {
+        val mather = UriMatcher(UriMatcher.NO_MATCH)
+        mather.addURI(authority,"book",bookDir)
+        mather.addURI(authority,"book/#",bookItem)
+        mather.addURI(authority,"category",categoryDir)
+        mather.addURI(authority,"category/#",categoryItem)
+        mather
+    }
+
 
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
         TODO("Implement this to handle requests to delete one or more rows")
     }
 
-    override fun getType(uri: Uri): String? {
-        TODO(
-            "Implement this to handle requests for the MIME type of the data" +
-                    "at the given URI"
-        )
+    override fun getType(uri: Uri) = when (uriMatcher.match(uri)){
+        bookDir -> "vnd.android.cursor.dir/vnd.com.raofa.example.kotlinsample.provider.book"
+        bookItem -> "vnd.android.cursor.item/vnd.com.raofa.example.kotlinsample.provider.book"
+        categoryDir -> "vnd.android.cursor.dir/vnd.com.raofa.example.kotlinsample.provider.category"
+        categoryItem -> "vnd.android.cursor.item/vnd.com.raofa.example.kotlinsample.provider.category"
+        else -> null
     }
 
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
         TODO("Implement this to handle requests to insert a new row.")
     }
 
-    override fun onCreate(): Boolean {
-        TODO("Implement this to initialize your content provider on startup.")
-    }
+    override fun onCreate() = context?.let {
+        dbHelper = MyDatabaseHelper(it, "BookStore.db", 3)
+        true
+    } ?: false
+
 
     override fun query(
         uri: Uri, projection: Array<String>?, selection: String?,
         selectionArgs: Array<String>?, sortOrder: String?
-    ): Cursor? {
-        TODO("Implement this to handle query requests from clients.")
+    ) = dbHelper?.let {
+        val db = it.readableDatabase
+        val cursor = when (uriMatcher.match(uri)){
+            bookDir -> db.query("book",projection,selection,selectionArgs,null,null,sortOrder)
+            bookItem -> {
+                val bookId = uri.pathSegments[1]
+                db.query("book",projection,"id = ?", arrayOf(bookId),null,null,sortOrder)
+            }
+            categoryDir -> db.query("category",projection,selection,selectionArgs,null,null,sortOrder)
+            categoryItem -> {
+                val categoryId = uri.pathSegments[1]
+                db.query("category",projection,"id = ?", arrayOf(categoryId),null,null,sortOrder)
+            }
+            else -> null
+        }
+        cursor
     }
 
     override fun update(
